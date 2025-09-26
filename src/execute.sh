b4d62ce8d2d4c53389c1bb1da40eace2b688b1dc
jguillermofc <jguillermo.fc@gmail.com>
2025-09-26 16:56:36 -0700

    inyectando [i] en pop inicial

--- src/execute.sh
+++ src/execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 hyperheuristic.py --N 10 --n 100 --Gmax 100 --M 10000 --m 3 --ppf RSE --subset_size 105 --iterations 10000 --QI SPD --runs_ss 11 --fitness SDD
+python3 hyperheuristic.py --N 10 --n 100 --Gmax 100 --M 10000 --m 3 --ppf RSE --subset_size 105 --iterations 10000 --QI SPD --runs_ss 11 --fitness SDD --runs_ga 5
